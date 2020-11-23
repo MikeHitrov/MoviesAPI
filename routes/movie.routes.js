b4a384e2b7775dfdb1c054740ac451385d5568f6
Mike Hitrov <mike.hitrov8892@gmail.com>
2020-11-23 15:36:01 +0200

    Get movie by id endpoint

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const movie = require("../models/movie.model");
-const m = require("../helpers/middlewares");
+const middlewares = require("../helpers/middlewares");
 
 // Routes
 module.exports = router;
@@ -10,6 +10,20 @@ router.get("/", async (req, res) => {
   await movie
     .getMovies()
     .then((movies) => res.json(movies))
+    .catch((err) => {
+      if (err.status) {
+        res.status(err.status).json({ message: err.message });
+      } else {
+        res.status(500).json({ message: err.message });
+      }
+    });
+});
+
+router.get("/:id", middlewares.mustBeInteger, async (req, res) => {
+  const id = req.params.id;
+  await movie
+    .getMovie(id)
+    .then((movie) => res.json(movie))
     .catch((err) => {
       if (err.status) {
         res.status(err.status).json({ message: err.message });
