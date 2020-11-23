bc3d4e33aef0f169a8af9c5265fed621918f159f
Mike Hitrov <mike.hitrov8892@gmail.com>
2020-11-23 14:56:06 +0200

    Made the structure and basic CRUD

---
+++ helpers/middlewares.js
@@ -0,0 +1,22 @@
+function mustBeInteger(req, res, next) {
+  const id = req.params.id;
+
+  if (!Number.isInteger(parseInt(id))) {
+    res.status(400).json({ message: "ID must be an integer" });
+  } else {
+    next();
+  }
+}
+function checkFieldsPost(req, res, next) {
+  const { title, director, releaseDate, genre, actors } = req.body;
+
+  if (title && director && releaseDate && genre && actors) {
+    next();
+  } else {
+    res.status(400).json({ message: "fields are not good" });
+  }
+}
+module.exports = {
+  mustBeInteger,
+  checkFieldsPost,
+};
---
+++ models/movie.model.js
@@ -0,0 +1,80 @@
+const filename = "../data/movies.json";
+let movies = require(filename);
+const helper = require("../helper.js");
+
+function getMovies() {
+  return new Promise((resolve, reject) => {
+    if (movies.length === 0) {
+      reject({
+        message: "No movies available!",
+        status: 202,
+      });
+    }
+    resolve(movies);
+  });
+}
+
+function getMovie(id) {
+  return new Promise((resolve, reject) => {
+    helper
+      .mustBeInArray(movies, id)
+      .then((movie) => resolve(movie))
+      .catch((err) => reject(err));
+  });
+}
+
+function insertMovie(newMovie) {
+  return new Promise((resolve, reject) => {
+    const id = { id: helper.getNewId(movies) };
+    const date = {
+      createdAt: helper.newDate(),
+      updatedAt: helper.newDate(),
+    };
+    newMovie = { ...id, ...date, ...newMovie };
+    movies.push(newMovie);
+    helper.writeJSONFile(filename, movies);
+    resolve(newMovie);
+  });
+}
+
+function updateMovie(id, newMovie) {
+  return new Promise((resolve, reject) => {
+    helper
+      .mustBeInArray(movies, id)
+      .then((movie) => {
+        const index = movies.findIndex((m) => m.id == movie.id);
+
+        id = { id: movie.id };
+        const date = {
+          createdAt: movie.createdAt,
+          updatedAt: helper.newDate(),
+        };
+
+        movies[index] = { ...id, ...date, ...newMovie };
+        helper.writeJSONFile(filename, movies);
+        resolve(movies[index]);
+      })
+      .catch((err) => reject(err));
+  });
+}
+
+function deleteMovie(id) {
+  return new Promise((resolve, reject) => {
+    helper
+      .mustBeInArray(movies, id)
+      .then(() => {
+        movies = movies.filter((m) => m.id !== id);
+        helper.writeJSONFile(filename, movies);
+        resolve();
+      })
+      .catch((err) => reject(err));
+  });
+}
+
+module.exports = {
+  insertMovie,
+  getMovies,
+  getMovie,
+  updateMovie,
+  deleteMovie,
+};
