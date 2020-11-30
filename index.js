85a848f4b4ccc52bc073f50a4d216ed05b98de5e
Mike Hitrov <mike.hitrov8892@gmail.com>
2020-11-30 14:31:11 +0200

    Moved the port into constant

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const port = 1337;
+
 const app = express();
+
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes/index.routes"));
-app.listen("1337");
+
+app.listen(port);
