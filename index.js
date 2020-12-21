f184a964ae2f287720b9abfce0cc22ce78e843e2
Mike Hitrov <mike.hitrov8892@gmail.com>
2020-12-21 23:16:35 +0200

    Added cors to the app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const port = 1337;
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("short"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
